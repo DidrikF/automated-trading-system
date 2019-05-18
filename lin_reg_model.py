ce16d6817e690db68466999ac2b4cc5e9af1b023
Didrik Fleischer <didrik.fleischer@gmail.com>
2019-05-18 17:26:16 +0200

    update

--- lin_reg_model.py
+++ lin_reg_model.py
@@ -6,6 +6,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn import linear_model
 from sklearn.model_selection import cross_val_predict # Don't know if I will use
 from sklearn.metrics import mean_squared_error, r2_score
+import numpy as np
 
 # Performance Metrics 
 from sklearn.metrics import mean_squared_error, mean_absolute_error
@@ -22,6 +23,7 @@ from dataset_columns import features, labels, base_cols
 from performance_measurement import zero_benchmarked_r_squared
 
 # DATASET PREPARATION
+print("Reading SEP")
 dataset = pd.read_csv("./dataset_development/datasets/completed/ml_dataset.csv", parse_dates=["date", "timeout"], index_col=["date"])
 dataset = dataset.loc[dataset.primary_label_tbm != 0]
 
@@ -37,17 +39,35 @@ std_scaler = StandardScaler()
 dataset_x = std_scaler.fit_transform(dataset_x) 
 
 # Encoding Categorical Features: NOTE: Not using industry now
-pca = PCA()
-reduced_dataset_x = pca.fit_transform(dataset_x)
+pca = PCA(n_components=5) # NOTE: How many principal complents?
+reduced_dataset = pca.fit_transform(dataset_x)
 
-train_start = dataset.index.min() # Does Date index included in features when training a model?
+
+ex_variance=np.var(reduced_dataset,axis=0)
+ex_variance_ratio = ex_variance/np.sum(ex_variance)
+
+# correlation_matrix =np.corrcoef(reduced_dataset)
+# print("Correlation Matrix")
+# print(correlation_matrix)
+
+
+reduced_dataset = pd.DataFrame(index=dataset.index, data=reduced_dataset)
+print(reduced_dataset)
+
+reduced_dataset["erp_1m"] = dataset["erp_1m"]
+
+
+train_start = reduced_dataset.index.min() # Does Date index included in features when training a model?
 train_end = pd.to_datetime("2012-01-01")
 
 test_start = pd.to_datetime("2012-03-01")
-test_end = dataset.index.max()
+test_end = reduced_dataset.index.max()
 
 train_set = dataset.loc[(dataset.index >= train_start) & (dataset.index < train_end)] # NOTE: Use for labeling and constructing new train/test sets
 test_set = dataset.loc[(dataset.index >= test_start) & (dataset.index <= test_end)] # NOTE: Use for labeling and constructing new train/test sets
+
+# Need to select the top principal components...
+
 
 train_x = train_set[features]
 train_y = train_set["erp_1m"] # Need to add erp_1m to the dataset, can be done in finalize_dataset, but maybe just rebuild the dataset first, only need to run the last task to add dividend adjusted labels_tbm
@@ -64,7 +84,7 @@ regressor.fit(train_x, train_y)
 # Calculate Performance measures
 test_x_pred = regressor.predict(test_x)
 r_squared = zero_benchmarked_r_squared(test_x_pred, test_y)
-
+print("Variance Ratios: ", ex_variance_ratio) 
 print("OOS Zero Benchmarked R-Squared: ", r_squared)
 print("OOS MSE: ", mean_squared_error(test_x_pred, test_y))
 print("OOS MAE: ", mean_absolute_error(test_x_pred, test_y))
